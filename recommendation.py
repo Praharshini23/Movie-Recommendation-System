7b7379ab24421aea54dff665fa595ba4ac1df0a2
Praharshini Seth <s.praharshini05@gmail.com>
2026-06-03 14:09:59 +0530

    recommendation.py

---
+++ recommendation.py
@@ -0,0 +1,36 @@
+# Recommendation Function
+# ----------------------------------
+
+def recommend_movies(movie_name, top_n=5):
+
+    if movie_name not in movie_genre_map:
+        return "Movie not found"
+
+    input_genres = movie_genre_map[movie_name]
+
+    recommended_genres = set()
+
+    for _, row in rules.iterrows():
+
+        for genre in input_genres:
+
+            if genre in row['antecedents']:
+                recommended_genres.update(
+                    row['consequents']
+                )
+
+    recommended_movies = []
+
+    for _, row in movies_df.iterrows():
+
+        if row['title'] != movie_name:
+
+            if any(
+                genre in recommended_genres
+                for genre in row['listed_in']
+            ):
+                recommended_movies.append(
+                    row['title']
+                )
+
+    return list(set(recommended_movies))[:top_n]
